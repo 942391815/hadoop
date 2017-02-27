c8a6f0a804a6595938d45cca5ae1c83ac4227c21
qiaogu <qiaogu@jd.com>
2017-02-27 21:09:12 +0800

    test filter

--- src/main/java/hbase/FilterTest.java
+++ src/main/java/hbase/FilterTest.java
@@ -5,9 +5,10 @@ import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.filter.*;
 import org.apache.hadoop.hbase.util.Bytes;
 
+
 /**
- * todo 过滤器深入理解
- *
+ * todo 过滤器深入理解  hbase 基于数值的过滤，分页PageFilter  自定义过滤器
+ * <p/>
  * create 'user','info'
  * Created by qiaogu on 2017/2/25.
  */
@@ -16,15 +17,50 @@ public class FilterTest {
         Connection connection = HbaseClient.getConnection();
 //        testQualiFiler(connection);
 //        testValueFilter(connection);
-        testDependentColumnFilter(connection);
+//        testDependentColumnFilter(connection);
+//        testPrefixFilter(connection);
+        testFilterList(connection);
+
+    }
+
+    public static void testFilterList(Connection connection) throws Exception {
+        Table table = connection.getTable(TableName.valueOf("user"));
+
+        FilterList filterList = new FilterList();
+        SingleColumnValueFilter singleColumnValueFilter = new SingleColumnValueFilter(Bytes.toBytes("info"),  //列族
+                Bytes.toBytes("age"),  //列名
+                CompareFilter.CompareOp.EQUAL, Bytes.toBytes("249"));
+
+        filterList.addFilter(new PrefixFilter(Bytes.toBytes("lisi9")));
+        filterList.addFilter(singleColumnValueFilter);
+
+        Scan scan = new Scan();
+        scan.setFilter(filterList);
+        printMessage(table.getScanner(scan));
+    }
+
+    /**
+     * 前缀过滤
+     *
+     * @param connection
+     * @throws Exception
+     */
+    public static void testPrefixFilter(Connection connection) throws Exception {
+        Table table = connection.getTable(TableName.valueOf("user"));
+        Scan scan = new Scan();
+        PrefixFilter prefix = new PrefixFilter(Bytes.toBytes("lisi9"));
+        scan.setFilter(prefix);
+
+        printMessage(table.getScanner(scan));
     }
 
     /**
      * 参考列过滤器
+     *
      * @param connection
      * @throws Exception
      */
-    public static void testDependentColumnFilter(Connection connection) throws Exception{
+    public static void testDependentColumnFilter(Connection connection) throws Exception {
         Table table = connection.getTable(TableName.valueOf("user"));
         Scan scan = new Scan();
         DependentColumnFilter dependentColumnFilter = new DependentColumnFilter(Bytes.toBytes("info"), Bytes.toBytes("age"));
@@ -35,6 +71,7 @@ public class FilterTest {
 
     /**
      * 查找age 等于240的数据
+     *
      * @param connection
      * @throws Exception
      */
@@ -51,6 +88,7 @@ public class FilterTest {
 
     /**
      * 列过滤器
+     *
      * @param connection
      * @throws Exception
      */
@@ -80,7 +118,11 @@ public class FilterTest {
 
     public static void printMessage(ResultScanner scanner) {
         for (Result res : scanner) {
-            System.out.println(res);
+//            List<Cell> columnCells = res.getColumnCells(Bytes.toBytes("info"), Bytes.toBytes("age"));
+//            for (Cell each:columnCells){
+//                System.out.println(Bytes.toString(each.getValueArray()));
+//            }
+            System.out.println(res+"-----"+res.getColumnCells(Bytes.toBytes("info"),Bytes.toBytes("age")));
         }
     }
 }
